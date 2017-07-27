db431f37c0f340c9235171595e96cf3e604393de
Justin Hernandez <jhernandezdev13@gmail.com>
2017-07-26 20:31:00 -0400

    Added calculator exercise from stanford ios course and beginner swift playground exercise

---
+++ playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+//: Here is a single line comment example
+ /*: This is
+ Multi line comment
+ # We are family
+ */
+
+
+
+import UIKit
+
+print("Welcome to swift")
+
+var x = 10
+
+for index in 1...20 {
+    let y = index * x
+    x -= 1
+}
+
+let myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+
+myLabel.backgroundColor = UIColor.red
+myLabel.text = "Hello Swift"
+myLabel.textAlignment = .center
+myLabel.font = UIFont(name: "Georgia", size: 24)
+myLabel
